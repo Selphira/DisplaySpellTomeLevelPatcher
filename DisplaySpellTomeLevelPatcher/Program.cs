e385d1a019d2856430b447cec6d6a4cd2a5372e5
Irwine <vandevelde632@gmail.com>
2021-06-12 00:10:22 +0200

    Update Program.cs

--- DisplaySpellTomeLevelPatcher/Program.cs
+++ DisplaySpellTomeLevelPatcher/Program.cs
@@ -70,8 +70,8 @@ namespace DisplaySpellTomeLevelPatcher
 
         public static string GetSpellNameFromScroll(string scrollName)
         {
-            string[] splitScrollName = scrollName.Split(' ');
-            string scrollSpellName = string.Join(' ', splitScrollName.Skip(2).ToArray());
+            string[] splitScrollName = scrollName.Split(' - ');
+            string scrollSpellName = string.Join(' - ', splitScrollName.Skip(2).ToArray());
             return scrollSpellName;
         }
         public static bool NamedFieldsContain<TMajor>(TMajor named, string str)
